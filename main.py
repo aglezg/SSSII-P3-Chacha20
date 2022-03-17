443db579e5884f41edf61a9fe619e99b89818669
Adrián González Galván <alu0101321219@ull.edu.es>
2022-03-17 00:46:06 +0000

    Completado - falta menu

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ CONSTANT_nd3 = '3320646e'
 CONSTANT_2by = '79622d32'
 CONSTANT_tek = '6b206574'
 
+ROUNDS = 20
+
 # Limpia la pantalla de la terminal
 def cleanTerminal():
   os.system('cls' if os.name == 'nt' else 'clear')
@@ -14,7 +16,7 @@ def cleanTerminal():
 # Invertir palabras de 32 bits (4 bytes)
 def invert(wordOf4bytes): 
   result = ''
-  aux = ''
+  aux = ''  
   it = 0
   while (it < len(wordOf4bytes)):
     if (it == 0 or it % 2 != 0):
@@ -51,8 +53,62 @@ def createInitialState(key, count, nonce):
 
   return initialState
 
+# ROTL
+def ROTL(a, b):
+  return ((a << b) & 0xffffffff) | a >> (32 - b)
+
+# QR
+def QR(x, a, b, c, d):
+  x[a] = (x[a] + x[b]) & 0xffffffff
+  x[d] ^= x[a]
+  x[d] = ROTL(x[d], 16)
+  
+  x[c] = (x[c] + x[d]) & 0xffffffff
+  x[b] ^= x[c]
+  x[b] = ROTL(x[b], 12)
+  
+  x[a] = (x[a] + x[b]) & 0xffffffff
+  x[d] ^= x[a]
+  x[d] = ROTL(x[d], 8)
+  
+  x[c] = (x[c] + x[d]) & 0xffffffff
+  x[b] ^= x[c]
+  x[b] = ROTL(x[b], 7)
+
+# chacha_block
+def chachaBlock(state):
+  original_state = []
+
+  for i in range(len(state)):
+    original_state.append(state[i]) 
+
+  round = 0
+  while round < ROUNDS:
+    # Odd round
+    QR(state, 0, 4, 8, 12)
+    QR(state, 1, 5, 9, 13)
+    QR(state, 2, 6, 10, 14)
+    QR(state, 3, 7, 11, 15)
+    # Even round
+    QR(state, 0, 5, 10, 15)
+    QR(state, 1, 6, 11, 12)
+    QR(state, 2, 7, 8, 13)
+    QR(state, 3, 4, 9, 14)
+  
+    round += 2
+  
+  printState(state)
+  print('\n')
+
+  result = []
+  for i in range(len(state)):
+   result.append((original_state[i] + state[i]) & 0xffffffff)
+   
+  printState(result)
 
 state = createInitialState(['00010203', '04050607', '08090a0b', '0c0d0e0f',
   '10111213', '14151617', '18191a1b', '1c1d1e1f'], '01000000', ['00000009',
   '0000004a', '00000000'])
 printState(state)
+print('\n')
+chachaBlock(state)
